365a198a6b59acd0bdea8ece747adb287285a40e
Ian Acierto <ianacierto@Ians-MacBook-Pro.local>
2022-09-14 20:25:08 -0700

    commit before pulling main

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/homepage', (req, res)=>{
+//MIDDLEWARE?
+app.get('/', (req, res)=>{
   console.log('hit the route')
   res.json({
     message: 'homepage'
   })
 })
+
 app.listen(3001, ()=>{
   console.log('listening on port 3001')
 })
