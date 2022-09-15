0351b7c8e3fbd73d0942191eab58a530a90c8a99
Ryan Wendell <Ryan.wendell@me.com>
2022-09-14 18:51:15 -0700

    Added test page for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from 'react'
+import './App.css';
+import Item from './components/Item';
+import NavBar from './components/NavBar';
+
+
+
+let j = 0;
+
 function App() {
-
   const [routeMessage, setRouteMessage] = useState('')
-
   //connection to back-end with useEffect
   //doesn't really work yet but we can start making our components
   useEffect(()=>{
@@ -11,14 +17,23 @@ function App() {
     //data passed in from back-end
     .then((data)=>setRouteMessage(data))
   },[])
-
   
+
   return (
-    <div className="App">
-      <h1>WE UP</h1>
-      <h1>{routeMessage}</h1>
+    <div id='App'>
+      <NavBar/>
+      {Cards()}
     </div>
   );
+}
+
+
+function Cards(){
+  let cards = [];
+  for(let i = 0; i <12; i++ ){
+    cards.push(<Item name = {'test'+i} price = {20.00+i*3} description = 'Description Description Description'  isInCart = {j}/>)
+  }
+  return <div id='ItemsList'> {cards} </div>
 }
 
 export default App;
