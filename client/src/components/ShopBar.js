7be7cca12ab56c483a2ecdef79ab52fc30eead02
Ryan Wendell <Ryan.wendell@me.com>
2022-09-17 14:04:17 -0700

    Setting up and styling item view page

--- client/src/components/ShopBar.js
+++ client/src/components/ShopBar.js
@@ -3,7 +3,7 @@ import React from "react"
 function ShopBar(props){
     return(
         <div id="InputBar">
-            <form action="/addtocart" method="POST">
+            <form action="/additem" method="POST">
                 <button name="id" value={props.id}>+</button>
             </form>
             <a href={`/products/${props.id}`}>
--- client/src/pages/ItemView.js
+++ client/src/pages/ItemView.js
@@ -3,34 +3,58 @@ import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import NavBar from "../components/NavBar"
 
-
+let test = {id:7, title:"Samsung Galaxy Book", description:"Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched", price:1499, discountPercentage:4.15, rating:4.25, stock:50,brand:"Samsung",category:"laptops",thumbnail:"https://dummyjson.com/image/i/products/7/thumbnail.jpg",images:["https://dummyjson.com/image/i/products/7/1.jpg","https://dummyjson.com/image/i/products/7/2.jpg","https://dummyjson.com/image/i/products/7/3.jpg","https://dummyjson.com/image/i/products/7/thumbnail.jpg"]}
 function ItemView(){
 
     const [data, setData] = useState(null)
     const {id} = useParams()
     console.log(id)
 
-    useEffect(() => {
-        fetch(`/${id}`)
-        .then((res) => res.json())
-        .then((data) => setData(data.testData))
-    }, []);
+    // useEffect(() => {
+    //     fetch(`/${id}`)
+    //     .then((res) => res.json())
+    //     .then((data) => setData(data.testData))
+    // }, []);
 
     return(
       <div id='App'>
         <NavBar/>
-        {!data ? 'Loading...': RenderPage(data)}
+        {RenderPage(test)}
+        {/* {!data ? 'Loading...': RenderPage(test)} */}
       </div>
     )
 }
 
 function RenderPage(data){
     return(
-        <div id="Item">
-            <img src={data.img} width={100} height={100}/>
-            <h2>{data.name}</h2>
-            <p>${data.price}</p>
-            <p>{data.description}</p>
+        <div id="ItemDisplay">
+            <div id="Picture">
+                <img src={data.images[0]}/> 
+            </div>
+            <div id="Info">
+                <h1>{data.title}</h1>
+
+                <div id="InfoSection">
+                    <h2>Rating: </h2>
+                    <p>{data.rating}</p>
+                </div>
+                
+                <div id="InfoSection">
+                    <h2>Price: </h2>
+                    <p>${data.price}</p>
+                </div>
+                
+                <div id="Description">
+                    <h2>Description</h2>
+                    <p>{data.description}</p>
+                </div>
+
+                <form action="/additem" method="POST">
+                    <button name="id" value={data.id}>+</button>
+                </form>
+            </div>
+            
+           
         </div>
     )
 }
