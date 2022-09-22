924f804f043390731b3e157a2a4bf2e423e9e2a4
Ryan Wendell <Ryan.wendell@me.com>
2022-09-21 21:09:32 -0700

    styling and setting up InCartCounter

--- client/src/components/InCartCount.js
+++ client/src/components/InCartCount.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react"
 
-function InCartCount(){
-    // useEffect(() => {
-    //     fetch(`/`)
-    //     .then((res) => res.json())
-    //     .then((data) => setData(data.testData))
-    // }, []);
+function InCartCount(props){
+
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch(`/incart/count/${props.id}`)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+    }, []);
 
     return(
         <>
-            <p>In Cart: 0</p>
+            <p>In Cart: {!data ? 'Loading...': data}</p>
         </>
     )
 }
