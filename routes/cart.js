14792f9ed9f94a7d844abc9a742b514d683dc1f2
IanAcierto <acierto_vincent@yahoo.com>
2022-09-22 22:05:06 -0700

    layed out todos for routes

--- routes/cart.js
+++ routes/cart.js
@@ -6,19 +6,27 @@ const axios = require('axios')
 router.route('/').get((req,res)=>{
   const results = db.query('SELECT * FROM cart')
   res.json(results)
+  //const cartList = [req.body.resultsId or something]
+  //return cartList to views
   //select everything from cart and put it in a json
 })
 
 router.get('/count/:id',(req,res)=>{
   //redirect to /api/products/:id
+  //const itemId = req.body.Id
+  //redirect to cartview with id
 })
 
 router.put('/count/:id',(req,res)=>{
   //add item to db
+  //const itemId = req.body.itemId
+  //add itemId to db
 })
 
 router.delete('/count/:id',(req,res)=>{
   //delete item from db
+  //const itemId = req.body.itemId
+  //remove itemId from db
 })
 
 module.exports = router
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const { getProducts } = require('../controllers/products')
 const router = express.Router()
-const axios = require('axios')
+const Axios = require('axios')
 router.route('/')
 
 router.get('/',(req,res)=>{
-  const products = res.json(axios.get('https://dummyjson.com/products'))
+  //having trouble getting request
+  //const query = req.body.query
+  //get dummyjson.com/products/search?q={query}
+  const products = res.json(Axios.get('https://dummyjson.com/products'))
   res.send(products)
 })
 
 router.get('/:id',(req,res)=>{
   console.log('get /products/id')
+  //const itemId = req.body.itemId
+  //get dummyjson.com/products/itemId
   //send query data up from form, insert it into dummyjson url?
 })
 
